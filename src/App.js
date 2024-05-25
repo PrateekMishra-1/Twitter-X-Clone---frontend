ea1324afeed16bed2e6f5ec8c1eec8376f885abe
PRATEEK MISHRA <153118081+PrateekMishra-1@users.noreply.github.com>
2024-05-25 18:20:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import Body from './components/Body';
 import { Toaster } from "react-hot-toast"
 
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://twitter-x-clone-backend.onrender.com"
+axios.defaults.withCredentials = true;
+
 function App() {
   return (
     <div className="App">
