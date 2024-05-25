274771d995a3396c9672903d1a4d1b6a80d0e10d
PRATEEK MISHRA <153118081+PrateekMishra-1@users.noreply.github.com>
2024-05-26 04:21:27 +0530

    Update useGetMyTweets.js

--- src/hooks/useGetMyTweets.js
+++ src/hooks/useGetMyTweets.js
@@ -6,7 +6,8 @@ import { getAllTweets } from "../redux/tweetSlice";
 
 const useGetMyTweets = (id) => {
     const dispatch = useDispatch();
-    const { refresh, isActive } = useSelector(store => store.tweet)
+    const { refresh, isActive } = useSelector(store => store.tweet);
+    
 
     const fetchMyTweets = async () => {
         try {
@@ -19,25 +20,23 @@ const useGetMyTweets = (id) => {
             console.log(error);
         }
     }
-    const followingTweetHandler = async () => {
+    const followingTweetHandler = async () => { 
         try {
             axios.defaults.withCredentials = true;
-            const res = await axios.get(`${TWEET_API_END_POINT}/followingtweets/${id}`)
-            dispatch(getAllTweets(res.data.tweets))
+            const res = await axios.get(`${TWEET_API_END_POINT}/followingtweets/${id}`);
+            console.log(res);
+            dispatch(getAllTweets(res.data.tweets));
         } catch (error) {
             console.log(error);
         }
     }
+
     useEffect(() => {
-        if(id){
-             if (isActive) {
+        if(isActive){
             fetchMyTweets();
-        }
-        else {
+        }else{
             followingTweetHandler();
         }
-        }
-       
-    }, [isActive, refresh]);
+    }, [isActive,refresh]);
 };
 export default useGetMyTweets;
